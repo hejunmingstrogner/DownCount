98a0c5123adff91fb9add38807563e5b86439dc2
何俊明 <hejunmingstronger@126.com>
2017-02-09 12:08:54 +0800

    Update DLCount.h

--- DLCount/DLCount.h
+++ DLCount/DLCount.h
@@ -1,13 +1,28 @@
 //
-//  DLCount.h
-//  DLCount
+//  downLoadParser.h
+//  iShow
 //
-//  Created by 何俊明 on 2017/2/9.
-//  Copyright © 2017年 Organization Name. All rights reserved.
+//  Created by 何俊明 on 2016/12/6.
+//  Copyright © 2016年 何俊明. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface DLCount : NSObject
+@protocol dwDelegate <NSObject>
+
+-(void)receiveDwProgress:(CGFloat)progress;
+-(void)receiveDwFailDownLoad;
+-(void)receiveDwunZip:(NSString *)unZipPath;
+-(void)receiveDwFailUnZip;
+@end
+
+@interface downLoadParser : NSObject
+
+@property (nonatomic , strong) NSMutableDictionary * dwingDict;         //  下载管理器
+@property (nonatomic , strong) NSFileManager       * fm;                //  文件管理对象
+
++(void)addDownload:(UIViewController <dwDelegate> *)vc TaskUrl:(NSString *)urlString;
+-(void)startDownLoadUrl:(NSString *)urlString WithObject:(UIViewController<dwDelegate> *)vc;
 
 @end
